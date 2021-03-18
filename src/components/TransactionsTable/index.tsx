ee898bafb21a843e4e4ecf452ca474bfcad9abb4
Emanuele Correa <manuncorrea@hotmail.com>
2021-03-18 14:00:26 -0300

    configurando o axios

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+
+export function TransactionsTable() {
+  
+  useEffect(() => {
+   api.get('transictions')
+    .then(response => console.log(response.data))
+  }, []);
+
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          <tr>
+            <td>Desevolvimento de websites</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="witdraw">R$1.100</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
